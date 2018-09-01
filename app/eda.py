a412f4dba4a7e074f1756adee68d088badbea41e
Chibuzor Agubulom <chibuzor.agubulom@gmail.com>
2018-09-01 14:05:42 -0500

    changed cols function to initial_cols and moved up list variables

--- app/eda.py
+++ app/eda.py
@@ -2,6 +2,39 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import confusion_matrix
 
+col_clean = ['body_length', 'channels', 'delivery_method', 'fb_published', 'gts',
+       'has_analytics', 'has_logo', 'listed', 'name_length', 'num_order',
+       'num_payouts', 'org_facebook', 'org_twitter', 'sale_duration',
+       'sale_duration2', 'show_map', 'user_age', 'sold', 'currency_AUD',
+       'currency_CAD', 'currency_EUR', 'currency_GBP', 'currency_MXN',
+       'currency_NZD', 'currency_USD', 'payout_type_ACH', 'payout_type_CHECK',
+       'payout_type_undefined', 'public_domain_False', 'public_domain_True']
+
+cols = ['approx_payout_date', 'body_length', 'channels', 'country',
+       'currency', 'delivery_method', 'description', 'email_domain',
+       'event_created', 'event_end', 'event_published', 'event_start',
+       'fb_published', 'gts', 'has_analytics', 'has_header', 'has_logo',
+       'listed', 'name', 'name_length', 'num_order', 'num_payouts',
+       'object_id', 'org_desc', 'org_facebook', 'org_name', 'org_twitter',
+       'payee_name', 'payout_type', 'previous_payouts', 'sale_duration',
+       'sale_duration2', 'show_map', 'ticket_types', 'user_age',
+       'user_created', 'user_type', 'venue_address', 'venue_country',
+       'venue_latitude', 'venue_longitude', 'venue_name', 'venue_state',
+       'sold', 'public_domain']
+
+money = ['currency_AUD','currency_CAD', 'currency_EUR', 'currency_GBP',
+             'currency_MXN', 'currency_NZD', 'currency_USD']
+
+
+label = {'label': {'fraudster_event': True, 'premium': False, 'spammer_warn': False, 'fraudster': True,
+                   'spammer_limited': False, 'spammer_noinvite': False, 'locked': False, 'tos_lock': False,
+                   'tos_warn': False, 'fraudster_att': True, 'spammer_web': False, 'spammer': False}}
+
+columns = ['approx_payout_date', 'country', 'description', 'email_domain', 'event_created', 'event_end',
+           'event_published', 'event_start', 'name', 'object_id', 'org_desc',
+           'org_name', 'payee_name', 'previous_payouts', 'ticket_types', 'user_created', 'user_type',
+           'venue_address', 'venue_country',
+           'venue_latitude', 'venue_longitude', 'venue_name', 'venue_state', 'has_header']
 
 def metrics(y_true, y_pred):
     tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
@@ -22,17 +55,7 @@ def total_sold(series):
     return total_sold
 
 
-def try_err(df):
-    cols = ['acct_type', 'approx_payout_date', 'body_length', 'channels', 'country',
-       'currency', 'delivery_method', 'description', 'email_domain',
-       'event_created', 'event_end', 'event_published', 'event_start',
-       'fb_published', 'gts', 'has_analytics', 'has_header', 'has_logo',
-       'listed', 'name', 'name_length', 'num_order', 'num_payouts',
-       'object_id', 'org_desc', 'org_facebook', 'org_name', 'org_twitter',
-       'payee_name', 'payout_type', 'previous_payouts', 'sale_duration',
-       'sale_duration2', 'show_map', 'user_age',
-       'user_created', 'user_type', 'venue_address', 'venue_country',
-       'venue_latitude', 'venue_longitude', 'venue_name', 'venue_state']
+def initialize_cols(df, cols):
 
     for col in cols:
         lst = df.columns
@@ -42,8 +65,6 @@ def try_err(df):
 
 
 def currency(df):
-    money = ['currency_AUD','currency_CAD', 'currency_EUR', 'currency_GBP',
-             'currency_MXN', 'currency_NZD', 'currency_USD']
     for country in money:
         try:
             df[country]
@@ -65,20 +86,7 @@ def clean_data(df):
     return
     DataFrame of numeric and boolean values
     """
-
-    label = {'label': {'fraudster_event': True, 'premium': False, 'spammer_warn': False, 'fraudster': True,
-                       'spammer_limited': False, 'spammer_noinvite': False, 'locked': False, 'tos_lock': False,
-                       'tos_warn': False, 'fraudster_att': True, 'spammer_web': False, 'spammer': False},
-                        'listed': {'y': 1, 'n': 0}, "payout_type": {"": "undefined"}}
-
-    columns = ['approx_payout_date', 'country', 'description', 'email_domain', 'event_created', 'event_end',
-               'event_published', 'event_start', 'name', 'object_id', 'org_desc',
-               'org_name', 'payee_name', 'previous_payouts', 'ticket_types', 'user_created', 'user_type',
-               'venue_address', 'venue_country',
-               'venue_latitude', 'venue_longitude', 'venue_name', 'venue_state', 'has_header']
-
-
-    df = try_err(df)
+    df = initialize_cols(df, cols)
     df.rename(index=str, columns={"acct_type": "label"}, inplace=True)
     try:
         df['sold'] = list(map(total_sold, df['ticket_types']))
@@ -91,9 +99,12 @@ def clean_data(df):
     try:
         df.replace(label, inplace=True)
     except:
-        df['label'] = False
+        df['label'] = 0
+    df.replace({'listed': {'y': 1, 'n': 0}}, inplace=True)
+    df.replace({"payout_type": {"": "undefined"}}, inplace=True)
     df = pd.get_dummies(df, columns=["currency", "payout_type", "public_domain"])
     df = currency(df)
     df.drop(columns=columns, axis='columns', inplace=True)
     df.dropna(axis="index", inplace=True)
+    df = initialize_cols(df, col_clean)
     return df
