56b1cad94200fadf0fa2b55297f3fd644c34fcc1
Chibuzor Agubulom <chibuzor.agubulom@gmail.com>
2018-09-08 20:31:52 -0500

    output predict proba, fraud_level to web

--- app/app.py
+++ app/app.py
@@ -1,28 +1,37 @@
 import pickle
-from fraud_model import MyModel, get_data
+from fraud_model import MyModel
+from predict import mongo_data, segment
+from eda import clean_data
 from flask import Flask, request
 from flask import render_template, flash, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Home')
+    return render_template("index.html", title="Home")
 
-@app.route('/prediction')
+@app.route("/prediction")
 def prediction():
-    return render_template('prediction.html', title='Home')
+    return render_template("prediction.html", title="Home")
 
-@app.route('/predict', methods=['POST'] )
+@app.route("/predict", methods=["POST"] )
 def predict():
-    X = str(request.form['user_input'])
-    with open('model.pkl', 'rb') as f:
+    df = mongo_data()
+    df.pop("_id")
+    df1 = clean_data(df)
+    X = df1.values
+    with open("model.pickle", "rb") as f:
         model = pickle.load(f)
 
-    pred = "Predictions: {}".format(" ".join(model.predict([X])))
-    return render_template('predict.html', title='Home', pred = pred)
+    df["probability"] = model.predict_proba_1(X)[:, 1].round(3)
+    df["fraud_level"] = df["probability"].apply(segment)
+    df2 = df[df["fraud_level"].isin(["low risk", "medium risk", "high risk"])]
+    df3 = df2[["object_id", "probability", "fraud_level"]]
+    return render_template("predict.html", title="Home", pred = df3)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     app.run(host='0.0.0.0', port=8080, debug=True)
--- app/predict.py
+++ app/predict.py
@@ -3,12 +3,13 @@ from eda import clean_data
 import pickle
 import time
 import requests
+from bson.objectid import ObjectId
 from fraud_model import MyModel
 
 from pymongo import MongoClient
 client = MongoClient()
 db = client.fraud
-
+pd.set_option("display.max_columns", 500)
 
 def live_data(n=0):
     api_key = 'vYm9mTUuspeyAWH1v-acfoTlck-tCxwTw9YfCynC'
@@ -27,28 +28,37 @@ def store_data(n=0):
             time.sleep(360)
 
 
-def main():
+def mongo_data():
     data = list(db.live_records.find())
-    df = pd.DataFrame(data, columns=data.keys())
-    df = clean_data(df)
+    df = pd.DataFrame(data)
+    return df
 
-    # Create features
-    X = df.values
-    return X
-
+def segment(x):
+    if x < 0.5:
+        return "cleared"
+    elif x < 0.65:
+        return "low risk"
+    elif x < 0.8:
+        return "medium risk"
+    else:
+        return "high risk"
 
 if __name__=="__main__":
-    store_data(1)
+    df = mongo_data()
+    df1 = clean_data(df)
+    df1.pop("_id")
+    X = df1.values
+    # store_data(1)
     # data = live_data()
     # X = main(data)
     #Unpickle the model
-    # with open('model.pkl', 'rb') as f:
-    #     model = pickle.load(f)
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
 
     #Make the predictions
-    # y_predict = model.predict(X)
-    # y_pred_proba = model.predict_proba_1(X)
-
+    y_predict = model.predict(X)
+    y_pred_proba = model.predict_proba_1(X)
+    print(y_pred_proba)
     # print(X)
     # print(y_predict)
     # #Create a Dataframe of predicted probabilities
